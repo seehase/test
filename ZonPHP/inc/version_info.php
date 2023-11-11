1d33cb52b17f83c0b4a8857ee677f73b885bd225
Holger Seehausen <seehausen@mediamarktsaturn.com>
2023-11-11 12:25:56 +0100

    test

---
+++ ZonPHP/inc/version_info.php
@@ -0,0 +1,8 @@
+<?php
+/********************************************************************
+ * Version
+ *********************************************************************/
+$version = "v3.2.3";
+
+// Change SessionId if needed e.g. if you run multi instances
+$zonPHPSessionID = "SOLAR_" . str_replace('.', '_', $version);
